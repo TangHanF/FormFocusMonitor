81c245fed556efc5fd6be139ab8fd01f5c8b2c31
TangHanF <guofu_gh@163.com>
2016-08-24 18:28:59 +0800

    初始版本

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace 监控哪个窗体获取焦点
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(12, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 172);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "焦点监控---唐寒枫";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 监控哪个窗体获取焦点
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetForegroundWindow();//获取当前激活窗口 
+
+        [DllImport("user32", SetLastError = true)]
+        public static extern int GetWindowText(
+        IntPtr hWnd, //窗口句柄
+        StringBuilder lpString, //标题
+        int nMaxCount //最大值
+        );
+        [DllImport("user32 ")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32 ")]
+        private static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        [DllImport("user32.dll")]
+        private static extern int GetClassName(
+            IntPtr hWnd, //句柄
+            StringBuilder lpString, //类名
+            int nMaxCount //最大值
+        );
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            IntPtr myPtr = GetForegroundWindow(); // 窗口标题
+            StringBuilder title = new StringBuilder(255);
+            GetWindowText(myPtr, title, title.Capacity); // 窗口类名
+            StringBuilder className = new StringBuilder(256);
+            GetClassName(myPtr, className, className.Capacity); 
+            label2.Text ="句柄值:"+ myPtr.ToString() + "\n" + "标题："+title.ToString() + "\n" +"窗体名："+ className.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //让窗体始终处于置顶状态
+            //IntPtr hDeskTop = FindWindow("Progman", "Program Manager");
+            //SetParent(this.Handle, hDeskTop);
+        }
+    }
+}
